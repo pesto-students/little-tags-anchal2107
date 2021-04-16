7cb9452f01f9392050d746cb555d487fdbe9ee5a
anchal2107 <anchal2107@yahoo.com>
2021-04-16 20:29:17 +0530

    removing some links

---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,8 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBxA_nuP2SUaM3R8XOxOGELlJz2gjQvP00",
+    authDomain: "little-tags-developement.firebaseapp.com",
+    projectId: "little-tags-developement",
+    storageBucket: "little-tags-developement.appspot.com",
+    messagingSenderId: "825856352798",
+    appId: "1:825856352798:web:de68d35ae067f15b09c40f"
+  };
---
+++ src/firebase/Readme.md
@@ -0,0 +1,1 @@
+>> npm i firebase // npm install firebase
