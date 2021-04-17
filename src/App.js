d500b5e264b39dd12515a0680738136ac0f7e824
Vishal Donga <vishaldonga811@gmail.com>
2021-04-17 21:35:15 +0530

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.scss";
 import MainContent from "./maincontent/MainContent";
 import Header from "./header/Header";
