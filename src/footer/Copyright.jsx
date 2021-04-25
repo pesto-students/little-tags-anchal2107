8c9d66c0adcbd677a7f6086b10ed2b9e18566b97
Vishal Donga <vishaldonga811@gmail.com>
2021-04-25 17:24:09 +0530

    Add responsive

--- src/footer/Copyright.jsx
+++ src/footer/Copyright.jsx
@@ -1,7 +1,40 @@
+import {
+  FaCcVisa,
+  FaCcMastercard,
+  FaCcPaypal,
+  FaCcAmazonPay,
+  FaCcAmex,
+  FaCcApplePay,
+  FaCcDiscover,
+  FaFacebookSquare,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaYoutube
+} from "react-icons/fa";
+
 function Copyright() {
   return (
     <div className="copyright">
-      <h4>Made with ❤ in Surat</h4>
+      <div className="flex footer-payment-div social">
+        <FaFacebookSquare />
+        <FaInstagram />
+        <FaTwitter />
+        <FaLinkedin />
+        <FaYoutube />
+      </div>
+      <h4>
+        Made with <span className="red">❤</span> in Surat
+      </h4>
+      <div className="flex footer-payment-div payment">
+        <FaCcVisa />
+        <FaCcMastercard />
+        <FaCcPaypal />
+        <FaCcAmazonPay />
+        <FaCcAmex />
+        <FaCcApplePay />
+        <FaCcDiscover />
+      </div>
     </div>
   );
 }
