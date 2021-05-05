8f5932f692b1f50837d26fd863bdb43ca8a71fef
Vishal Donga <vishaldonga811@gmail.com>
2021-05-05 23:30:05 +0530

    fix hamburger bug

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,23 +1,24 @@
 import React, { useState} from "react";
+import { useSelector } from "react-redux";
 import MenuIcon from "./MenuIcon";
 import CartIcon from "./CartIcon";
 import ProfileIcon from "./ProfileIcon";
 import SearchFilter from "./SearchFilter";
 import { NavLink } from "react-router-dom";
 import WishItemIcon from "./WishItemIcon";
-
 import logo from "./../assets/logoo.JPG";
 import "./Header.scss";
 import SignUpModal from "./authentication/SignUpModal";
 
 function Header() {
   const [showLogin, setShowLogin] = useState(false);
+  const authUser = useSelector((state) => state.sessionState);
   const showLoginModal = () => setShowLogin(!showLogin);
 
   return (
     <div className="header">
       {showLogin ? <SignUpModal handleCloseModal={showLoginModal}/> : null}
-      <MenuIcon />
+      <MenuIcon authUser={authUser}/>
       <div className="logo">
         <NavLink to={"/"}>
           <img src={logo} alt="logo" />
